385af650e42fe9321124287270e3886e88091fc6
Scott Horn <shorn813@outlook.com>
2018-06-13 19:41:17 -0400

    app is reset without trnasitions...figure that sh outgit add .

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from './Card';
-import { CSSTransitionGroup } from 'react-transition-group'
+// import { CSSTransitionGroup } from 'react-transition-group'
 
 const CardList = (props) => (
     <div>
